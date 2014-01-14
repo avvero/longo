0d1d595b9b2628e5c81bab05982521ca263f8456
unknown <fxdev-belyaev-ay@belyaev-au2.corp.local>
2014-01-14 14:23:25 +0700

    Перевел на ангулар

--- grails-app/views/flow/index.gsp
+++ grails-app/views/flow/index.gsp
@@ -20,12 +20,16 @@
 </head>
 <body>
     <div id="App1" ng-app="flow" ng-controller="flowController">
-        <h1>Your order</h1>
-        <div ng-repeat="item in items">
-            <span>{{item.product_name}}</span>
-            <span>{{item.price | currency}}</span>
-            <button ng-click="remove($index);">Remove</button>
-            <button ng-click="addItem()">Add to list</button>
+        <div ng-repeat="log in items">
+            <span>
+                {{log.date}}&nbsp;&nbsp;
+                {{log.level}}&nbsp;
+                {{log.thread}}&nbsp;
+                {{log.user}}&nbsp;
+                {{log.class.className}}:{{log.method}}:{{log.lineNumber}}&nbsp;
+                {{log.message}}&nbsp;
+            </span>
+            <div ng-bind-html-unsafe="log.messageLong"></div>
         </div>
     </div>
     <table style="font-size: small;">
--- target/work/tomcat/work/Tomcat/localhost/longo/grails-resources/js/flow.js
+++ target/work/tomcat/work/Tomcat/localhost/longo/grails-resources/js/flow.js
@@ -1,27 +1,125 @@
 
-var controller;
-var addItem;
+var canApply = true;
+function doApply() {
+    canApply = false;
+    setTimeout(function() { canApply = true }, 100);
+}
 
 $( document ).ready(function() {
     var flowModule = angular.module("flow", [])
-    controller = flowModule.controller("flowController",
+    flowModule.filter('reverse', function() {
+        return function(items) {
+            return items.slice().reverse();
+        };
+    });
+    flowModule.controller("flowController",
         function($scope) {
-            $scope.items = [
-                {product_name: "Product 1", price: 50},
-                {product_name: "Product 2", price: 50},
-            ];
+            $scope.items = [];
             $scope.remove = function(index) {
                 $scope.items.splice(index, 1);
             }
-            // Add a Item to the list
-            addItem = $scope.addItem = function () {
-
-                $scope.items.push({
-                    product_name: "Test",
-                    price: "10"
-                });
-
+            $scope.addLogEntry = function (log) {
+                $scope.items.push(log);
+                $scope.$apply();
             };
+            $scope.addLimitLogEntry = function (log) {
+                if ($scope.items.length > 30) {
+                    $scope.items.splice(0,1);
+                }
+                $scope.items.push(log);
+                if (canApply) {
+                    $scope.$apply();
+                    doApply();
+                }
+            };
+            //------------------
+            $scope.LogFlow = {
+                socket: null,
+                chatSubscription: null,
+                notificationSubscription: null,
+                publicSubscription: null,
+                transport: null,
+
+                subscribe: function (options) {
+                    var defaults = {
+                        contentType: "application/json",
+                        shared: false,
+                        transport: 'websocket',
+                        //transport: 'long-polling',
+                        fallbackTransport: 'long-polling',
+                        trackMessageLength: true
+                    }
+                    var logFlowRequest = $.extend({}, defaults, options);
+
+                    logFlowRequest.onOpen = function (response) {
+                        $scope.LogFlow.chatSubscription.push("{message: 'Im in!'}");
+                    };
+                    logFlowRequest.onReconnect = function (request, response) {
+                    };
+                    logFlowRequest.onMessage = function (response) {
+                        $scope.LogFlow.onMessage(response);
+                    };
+                    logFlowRequest.onError = function (response) {
+                    };
+                    logFlowRequest.onTransportFailure = function (errorMsg, request) {
+                    };
+                    logFlowRequest.onClose = function (response) {
+                    };
+                    $scope.LogFlow.chatSubscription = $scope.LogFlow.socket.subscribe(logFlowRequest);
+                },
+
+                unsubscribe: function () {
+                    $scope.LogFlow.socket.unsubscribe();
+                },
+
+                onMessage: function (response) {
+                    var data = $.parseJSON(response.responseBody);
+//                    var table = $('#logBody')
+//                    var tr = $( "<tr>")
+//                    // -----------
+//                    var level = data.level
+                    var date = moment(new Date(data.timestamp['$date'])).format("YYYY-MM-DD HH:mm:ss")
+                    data.date = date
+//                    var thread = data.thread
+                    var user = data.properties ? '('+data.properties.userLogin+','+data.properties.sessionId+')' : ""
+                    data.user = user
+//                    var method = data.class.className+":"+data.method+":"+data.lineNumber
+                    var message = data.message
+//                    // -----------
+//                    var log = date + "&nbsp;&nbsp;" + level + "&nbsp;" + thread + "&nbsp;" + user + "&nbsp;"
+//                        + method + "&nbsp;-&nbsp;"// + message
+//                    // -----------
+//                    table.prepend(tr)
+//                    //
+//                    // XXX Нормально парсим в строку нужно нам
+                    if ((message + " ").indexOf('\r\n')!= -1) {
+//                        var tr2 = $( "<tr>")
+//                        tr2.append($( "<td>"))
+                        message = safeTags(message)
+                        message = message.replaceAll('\r\n', '<br/>')
+//                        tr2.append($( "<td>").append(message))
+//                        table.prepend(tr2)
+                        data.message = ""
+                        data.messageLong = message
+                    }
+//                    // -----------
+//                    tr.append($( "<td colspan='2'>").append(log))
+//                    table.prepend(tr)
+
+                    $scope.addLimitLogEntry(data)
+                }
+            };
+            if (typeof atmosphere == 'undefined') {
+                // if using jquery.atmosphere.js
+                $scope.LogFlow.socket = $.atmosphere;
+            } else {
+                // if using atmosphere.js
+                $scope.LogFlow.socket = atmosphere;
+            }
+            var logFlowRequest = {
+                url: 'log/'  + id
+            };
+            $scope.LogFlow.subscribe(logFlowRequest);
         }
     );
 });
